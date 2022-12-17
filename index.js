a33bfc886190b512b082c704c284864b0a06897d
Abhyudya Bajpai <42513946+Abhyudyabajpai@users.noreply.github.com>
2022-12-17 17:57:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const PORT = 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+
+app.get('/', (req,res) => {
+    res.json('Welcome to my climate news API')
+})
+
+app.get('/news', (req,res) =>{
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+    .then((response) => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate)',html).each(function (){
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            
+        })
+    })
+})
