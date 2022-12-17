440a454c5743917e3dd31129654596ea4e9a10ff
Abhyudya Bajpai <42513946+Abhyudyabajpai@users.noreply.github.com>
2022-12-17 18:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cheerio = require('cheerio')
 const app = express()
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
-
+const articles = []
 app.get('/', (req,res) => {
     res.json('Welcome to my climate news API')
 })
@@ -17,10 +17,14 @@ app.get('/news', (req,res) =>{
         const html = response.data
         const $ = cheerio.load(html)
 
-        $('a:contains("climate)',html).each(function (){
+        $('a:contains("climate")',html).each(function (){
             const title = $(this).text()
             const url = $(this).attr('href')
-            
+            articles.push({
+                title,
+                url
+            })
         })
-    })
+        res.json(articles)
+    }).catch((err) => console.log(err) )
 })
